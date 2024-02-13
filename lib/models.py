c3618de3a2c5fd8c1632365bae266734e00ac31b
Teklu Mezgebo <mezge002@umn.edu>
2024-02-13 09:24:12 -0600

    many to many relationship created between tables

--- lib/models.py
+++ lib/models.py
@@ -10,6 +10,13 @@ metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
 
+game_user = Table(
+    'game_users',
+    Base.metadata,
+    Column('game_id', ForeignKey('games.id'), primary_key=True),
+    Column('user_id', ForeignKey('users.id'), primary_key=True)
+)
+
 class Game(Base):
     __tablename__ = 'games'
 
@@ -19,7 +26,7 @@ class Game(Base):
     platform = Column(String())
     price = Column(Integer())
 
-    reviews = relationship('Review', backref=backref('game'))
+    users = relationship('User', secondary=game_user, back_populates='games')
 
     def __repr__(self):
         return f'Game(id={self.id}, ' + \
@@ -32,10 +39,24 @@ class Review(Base):
     id = Column(Integer(), primary_key=True)
     score = Column(Integer())
     comment = Column(String())
-    
+    user_id = Column(Integer(), ForeignKey('users.id'))
     game_id = Column(Integer(), ForeignKey('games.id'))
 
     def __repr__(self):
         return f'Review(id={self.id}, ' + \
             f'score={self.score}, ' + \
             f'game_id={self.game_id})'
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    created_at = Column(DateTime(), server_default=func.now())
+    updated_at = Column(DateTime(), onupdate=func.now())
+
+    games = relationship('Game', secondary=game_user, back_populates='users')
+
+    def __repr__(self):
+        return f'User(id={self.id}, ' + \
+            f'name={self.name}'
